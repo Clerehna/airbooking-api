258b3924d5decdaf8b130a5b805f5038aeee111f
Charlene Nicol <charln.nicol@gmail.com>
2022-12-15 12:04:57 +0100

    controller listings and seed created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'listings/index'
+  get 'listings/show'
+  get 'listings/create'
+  get 'listings/edit'
+  get 'listings/update'
+  get 'listings/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Nettoyage de la DB 🧹'
+
 Booking.delete_all
 Reservation.delete_all
 Mission.delete_all
 Listing.delete_all
 
-Listing.create!(num_rooms: 2)
-Listing.create!(num_rooms: 1)
-Listing.create!(num_rooms: 3)
+puts 'Creation de room 🔲'
+
+Listing.create!(num_room: 2)
+Listing.create!(num_room: 1)
+Listing.create!(num_room: 3)
+
+puts 'Creation de booking 📆'
 
 Booking.create!(listing_id: Listing.first.id, start_date: "2016-10-10".to_date, end_date: "2016-10-15".to_date)
 Booking.create!(listing_id: Listing.first.id, start_date: "2016-10-16".to_date, end_date: "2016-10-20".to_date)
 Booking.create!(listing_id: Listing.second.id, start_date: "2016-10-15".to_date, end_date: "2016-10-20".to_date)
+
+puts 'Creation de reservation 🗓'
 
 Reservation.create!(listing_id: Listing.first.id, start_date: "2016-10-11".to_date, end_date: "2016-10-13".to_date)
 Reservation.create!(listing_id: Listing.first.id, start_date: "2016-10-13".to_date, end_date: "2016-10-15".to_date)
